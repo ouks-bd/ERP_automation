79bbd7d3c4867b6656ebdab218291957ee487a30
ouks-bd <obyed.oss@gmail.com>
2022-03-07 10:55:32 +0600

    first

--- src/main/java/testcase/Testcaselocator.java
+++ src/main/java/testcase/Testcaselocator.java
@@ -38,7 +38,7 @@ public class Testcaselocator extends DriverSetup {
 		
 		driver.get("http://test.beacontech.xyz/merchandising/order");
 		driver.findElement(By.id("iconName")).click();
-		
+		Thread.sleep(2000); 
 		
 		WebElement element = driver.findElement(By.id("ddlBuyer"));
 		Select select =new Select(element);
@@ -50,22 +50,30 @@ public class Testcaselocator extends DriverSetup {
 		Thread.sleep(2000); 
 		driver.findElement(By.id("txt5")).sendKeys("2022");
 		driver.findElement(By.id("Commission")).sendKeys("5");
+		driver.findElement(By.id("txt6")).sendKeys("Automation");
 		Thread.sleep(5000); 
-		//driver.findElement(By.xpath("//div[@class='col-lg-12']//button[@id='btnsave']")).click();
+		
+		// located element with contains()
+		//driver.findElement(By.xpath("//*[contains(text(),'ADD')]"));
+		
+		WebElement elementadd = driver.findElement(By.id("btnsave"));
+		elementadd.submit();
+		
+//		driver.findElement(By.id("btnsave")).click(); 
 //		Thread.sleep(2000);
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("document.querySelector('body > div:nth-child(1) > div:nth-child(3) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > form:nth-child(1) > div:nth-child(2) > div:nth-child(6) > div:nth-child(1) > button:nth-child(1)').click();");
-		Thread.sleep(2000);
-//		System.out.println("ok");
-		//driver.findElement(By.id("btnsave")).click(); 
 		//driver.findElement(By.cssSelector("input[type='button']")).click()
 		//driver.findElement(By.className("button[@class='col-lg-12']"));
 		//driver.findElement(By.id("//button[@id='btnsave']")).click(); 
+		//driver.findElement(By.xpath("//div[@class='col-lg-12']//button[@id='btnsave']")).click();
+//		Thread.sleep(2000);
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//		js.executeScript("document.querySelector('...').click();");
 		
+//		System.out.println("ok");
 		
 		
 		//driver.findElement(By.xpath("(//a[starts-with(@href,'/Merchandising/OrderView/')])[594]")).click();
-		/**
+		
 		driver.findElement(By.xpath("(//a[starts-with(@href,'/Merchandising/OrderView/')])[1]")).click();
 		
 		//driver.get ("http://test.beacontech.xyz/Merchandising/OrderView/594");
@@ -78,7 +86,7 @@ public class Testcaselocator extends DriverSetup {
 		
 		
 		driver.findElement(By.xpath("//input[@id='vmStyle_Reference']")).sendKeys("PO002");
-		Thread.sleep(2000); 
+		Thread.sleep(2000);
 		
 		
 		//Finish Category★ 
@@ -122,9 +130,30 @@ public class Testcaselocator extends DriverSetup {
 		colour.selectByValue("2");
 		Thread.sleep(2000);
 		
+		
+		// Create WebElement
+		//WebElement eleClear = driver.findElement(By.id("vmStyle_PackQty")); 
+		// Perform clear operation
+		//eleClear.clear();
+		// OR
+		// Clear particular WebElement e.g: Textbox.
+		driver.findElement(By.id("vmStyle_PackQty")).clear();
+		//driver.findElement(By.id("vmStyle_PackQty")).clear();
+		
 		driver.findElement(By.xpath("//input[@id='vmStyle_PackQty']")).sendKeys("70000");
+		
+		driver.findElement(By.id("vmStyle_PackPieceQty")).clear();
 		driver.findElement(By.xpath("//input[@id='vmStyle_PackPieceQty']")).sendKeys("2");
+		
+//		WebElement packprice = driver.findElement(By.id("vmStyle_PackPrice")); 
+//		// Perform clear operation
+//		packprice.clear();
+//		// OR
+//		// Clear particular WebElement e.g: Textbox.
+		driver.findElement(By.id("vmStyle_PackPrice")).clear();
 		driver.findElement(By.xpath("//input[@id='vmStyle_PackPrice']")).sendKeys("7");
+		
+		driver.findElement(By.id("vmStyle_SetQuantity")).clear();
 		driver.findElement(By.xpath("//input[@id='vmStyle_SetQuantity']")).sendKeys("2");
 		Thread.sleep(2000);
 		
@@ -151,22 +180,24 @@ public class Testcaselocator extends DriverSetup {
 		BOM_Fabric_c.selectByValue("1");
 		
 		
-		
+		//driver.findElement(By.id("vmBOM.GSM")).clear();
 		driver.findElement(By.xpath("//input[@name='vmBOM.GSM']")).sendKeys("160");
 		Thread.sleep(2000);
 		
 		//driver.findElement(By.id("txt5")).sendKeys("500");
 		
-		
+		//driver.findElement(By.id("vmBOM.Consumption")).clear();
 		WebElement consumption=driver.findElement(By.name("vmBOM.Consumption"));
         consumption.sendKeys("500");
         
+        //driver.findElement(By.id("vmBOM.ConRequiredQty")).clear();
         WebElement required_quantity=driver.findElement(By.name("vmBOM.ConRequiredQty"));
         required_quantity.sendKeys("5");
         
 		WebElement tolerence=driver.findElement(By.name("vmBOM.Tolerance"));
         tolerence.sendKeys("0.1");
      
+        //driver.findElement(By.id("vmBOM.UnitPrice")).clear();
         WebElement UnitPrice=driver.findElement(By.name("vmBOM.UnitPrice"));
         UnitPrice.sendKeys("10");
         Thread.sleep(2000);
@@ -174,14 +205,19 @@ public class Testcaselocator extends DriverSetup {
         driver.findElement(By.id("btnsave")).click();
         
         
+       
         WebElement sewing = driver.findElement(By.id("ItemFK_6"));
 		Select lab  =new Select(sewing);
-		lab.selectByValue("6786");
+		lab.selectByValue("14");
 		Thread.sleep(2000);
 		
-		//(//input[@name='vmBOM.Tolerance'])[2]
+//		WebElement sewing_colour = driver.findElement(By.name("//select[@name='vmBOM.Common_ColorFK'])[2]"));
+//		Select sewing_value  =new Select(sewing_colour);
+//		sewing_value .selectByValue("1");
+//		Thread.sleep(5000);
 		
-		WebElement Fabric_colour_sewing = driver.findElement(By.xpath("//select[@name='vmBOM.Common_ColorFK'])[2]"));
+		
+		WebElement Fabric_colour_sewing = driver.findElement(By.id("//select[@name='Common_ColorFK'])[2]"));
 		Select BOM_color_sewing  =new Select(Fabric_colour_sewing);
 		BOM_color_sewing.selectByValue("1");
 		Thread.sleep(5000);
@@ -201,6 +237,9 @@ public class Testcaselocator extends DriverSetup {
         
         driver.findElement(By.id("btnsave")).click();
         Thread.sleep(4000);
+        
+        
+        
 //		
 //		//driver.findElement(By.cssSelector("p.error")).getText(); 
 ////		formula : tagname.value(if class is given)
@@ -238,40 +277,14 @@ public class Testcaselocator extends DriverSetup {
 		
 		
 		
-		//select.selectByIndex(3);
-//		select.selectByVisibleText("PRIMARK LIMITED");
+
 		
-//		
-		
-		//driver.findElement(By.className("select2-selection__rendered")).click();
-		
-		//WebElement selectElement = driver.findElement(By.cssSelector("select2-ddlBuyer-container"));
+
 		
 		
 		
-		//driver.findElement(By.id("select2-ddlBuyer-container"));
-		
-//		Select selectObject = new Select(selectElement);
-//		  
-//		//select.selectByVisibleText("PRIMARK LIMITED");
-//		selectObject.selectByIndex(1);
-		
-		
-		
-		
-//		driver.findElement(By.id("select2-ddlBuyer-container")).click();
-//		
-//		
-//		List <WebElement> listitem = driver.findElements(By.id("li[id*='select2-ddlBuyer-container']"));
-//		for (WebElement item : listitem) {
-//			//System.out.print("item"); 
-//			if (item.getText().equals("EDGE")) {
-//				item.click();
-//			}
-//		}
-//		
- * 
- */
+	
+
 	}
 
 }
